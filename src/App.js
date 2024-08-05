0a40337238e123e7570a5fcdea429ac819f805fe
Carlos Legnaro de Almeida <carlos_legnaro@outlook.com>
2024-08-05 15:09:02 -0300

    Correção do Exercício

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 
-let initialState = [
-  [
+let initialState = [  
     {
       "id": 1,
       "descricao":"Primeira Atividade"
@@ -10,8 +9,7 @@ let initialState = [
     {
       "id": 2,
       "descricao":"Segunda Atividade"
-    },
-  ]
+    },  
 ];
 
 function App() {
@@ -26,23 +24,22 @@ function App() {
       id: document.getElementById('id').value,
       descricao: document.getElementById('descricao').value,
     };
-    atividades.push(atividade);
-    console.log(atividades);
-    setAtividades([...atividades]);
+    
+    setAtividades([...atividades, {...atividade}]);
 
   }
     
   return (
 <>
     <form>
-        <input id="id" type="text" placeholder="id" />
-        <input id="descricao" type="text" placeholder="descricao" />
+        <input id='id' type='text' placeholder='id' />
+        <input id='descricao' type='text' placeholder='descricao' />
         <button onClick={addAtiviade}>+ Atividade</button>
     </form>
-    <div className="mt-3">             
-        <ul class="list-group">
-          {atividades.map( ativ =>(
-            <li key={ativ.id} className="list-group-item">
+    <div className='mt-3'>             
+        <ul class='list-group'>
+          {atividades.map( ativ => (
+            <li key={ativ.id} className='list-group-item'>
               {ativ.id} - {ativ.descricao}
             </li>
           ))}                   
