ae993c816976ab36b5213dc984efb58b7a7f0e67
carlos.legnaro <carlos.legnaro@outlook.com>
2026-02-21 21:21:26 -0300

    :: versão limpa e melhorada do código ::

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const atividades = [
+    {
+      id: 1,
+      descricao: 'Primeira Atividade',
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <ul>
+          <li>
+            {atividades[0].id} {atividades[0].descricao}
+          </li>
+          <li onClick={() => console.log('JSX')}>Segunda Atividade</li>
+          <li>Terceira Atividade</li>
+          <li>Quarta Atividade</li>
+        </ul>
       </header>
     </div>
   );
